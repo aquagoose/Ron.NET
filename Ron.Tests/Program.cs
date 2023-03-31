e8f2c7f30670d7743621fe171def24bf86cd4658
ollie <ollie@ollierobinson.co.uk>
2023-03-31 17:29:34 +0100

    started rongen

--- Ron.Tests/Program.cs
+++ Ron.Tests/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using Ron.NET;
 using Ron.Tests;
+using RonGen;
 
-string ron = @"
+/*string ron = @"
 Stuff(
     Number: 3,
     String: ""This is a test!"",
@@ -17,4 +18,24 @@ Element element = Ron.NET.Ron.Parse(ron);
 Console.WriteLine(element["Stuff"]["String"]);
 Console.WriteLine(element["Stuff"]["Struct"]["W"]);
 Console.WriteLine();
-Console.WriteLine(element);
+Console.WriteLine(element);*/
+
+Console.WriteLine(Generator.GenerateDeserializerMethodForType(typeof(TestClass)));
+
+TestClass obj = Deserialize_Ron_Tests_TestClass(@"
+Hello: 22,
+Test: ""This is some text."",
+Thing: true");
+
+Console.WriteLine(obj);
+
+static Ron.Tests.TestClass Deserialize_Ron_Tests_TestClass(string ron)
+{
+    Element element = Ron.NET.Ron.Parse(ron);
+    var obj = new Ron.Tests.TestClass();
+    obj.Hello = element["Hello"].GetValue<System.Int32>();
+    obj.Test = element["Test"].GetValue<System.String>();
+    obj.Thing = element["Thing"].GetValue<System.Boolean>();
+
+    return obj;
+}
---
+++ RonGen/Generator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace RonGen;
+
+public static class Generator
+{
+    public static string GenerateDeserializerMethodForType(Type type, string methodName = null)
+    {
+        methodName ??= $"Deserialize_{type.FullName.Replace('.', '_')}";
+
+        StringBuilder builder = new StringBuilder();
+        builder.AppendLine($"public static {type.FullName} {methodName}(string ron)" + "\n{");
+        builder.AppendLine("    " + 
+            GenerateDeserializerForType(type)
+                .Replace("\n", "\n    ")
+                .Replace("<<RON_SERIALIZER_TEXT>>", "ron")
+            );
+        builder.AppendLine("    return obj;\n}");
+        return builder.ToString();
+    }
+    
+    public static string GenerateDeserializerForType(Type type)
+    {
+        foreach (ConstructorInfo info in type.GetConstructors())
+        {
+            if (info.GetParameters().Length == 0)
+                goto SKIP_ERROR;
+        }
+
+        throw new MissingMethodException(
+            "Type is missing parameterless constructor which is required for initialization.");
+        
+        SKIP_ERROR:
+        StringBuilder code = new StringBuilder("Element element = Ron.NET.Ron.Parse(<<RON_SERIALIZER_TEXT>>);\n");
+        
+        code.AppendLine($"var obj = new {type.FullName}();");
+
+        foreach (FieldInfo field in type.GetFields(BindingFlags.Public | BindingFlags.Instance))
+        {
+            code.Append("obj." + field.Name + " = ");
+            code.AppendLine($"element[\"{field.Name}\"].GetValue<{field.FieldType.FullName}>();");
+        }
+
+        return code.ToString();
+    }
+}
